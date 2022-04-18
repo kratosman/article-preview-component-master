3870eb757ee3c71ea9cf6e4385179b15c0cf61d0
benjiesilvano4@gmail.com <103920174+kratosman@users.noreply.github.com>
2022-04-19 06:55:29 +0800

    first commit

--- app.js
+++ app.js
@@ -5,9 +5,3 @@ open.addEventListener('click', () => {
     show.classList.toggle("show-open");
     show.classList.toggle("active");
 });
-
-// window.onclick = function(e) {
-//     if (e.target == show) {
-//         console.log(show.style.display = "none");
-//     }
-// }
